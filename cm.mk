cfd075328854395e131bec5979934bf135e21c6b
SerenityS <jins4218@naver.com>
2016-10-24 23:26:37 +0900

    cm: Inherit full_base_telephony

--- cm.mk
+++ cm.mk
@@ -15,7 +15,9 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 $(call inherit-product, device/samsung/i9100/i9100.mk)
 
 # Release name
